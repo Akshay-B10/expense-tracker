0de4e5e785d2c819c7e3564790cb1b56a68cf52d
Akshay-B10 <akshaybhokare777@gmail.com>
2023-07-27 23:05:58 +0530

    helmet, morgan, write log in file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const fs = require("fs");
 
 require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const sequelize = require("./utils/config");
 const adminRoutes = require("./routes/admin");
@@ -21,9 +24,23 @@ const Downloads = require("./models/downloads");
 
 const app = express();
 
-app.use(bodyParser.json({extended: false}));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+    flags: "a" 
+});
+
+app.use(bodyParser.json({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://checkout.razorpay.com/", "https://cdnjs.cloudflare.com/", "https://api.razorpay.com"], // due to error: failed to load script
+        frameSrc: ["'self'", "https://api.razorpay.com"] // due to error: refused to frame
+    }
+}));
+app.use(morgan("combined", {
+    stream: accessLogStream
+}));
 
 app.use(adminRoutes);
 
